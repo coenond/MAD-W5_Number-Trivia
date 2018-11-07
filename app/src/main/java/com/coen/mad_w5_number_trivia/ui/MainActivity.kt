c48b4b695c6e7007015009940804f436d7dbe680
Coen Onderwater <coenonderwater@gmail.com>
2018-11-07 12:06:16 +0100

    add project dependencies

---
+++ app/src/main/java/com/coen/mad_w5_number_trivia/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.coen.mad_w5_number_trivia.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.coen.mad_w5_number_trivia.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
